2d68b9ebeab4eee565f403a988d1c5b4f9fe53e9
Tabassum <tabbasum.ss@gmail.com>
2026-02-10 12:47:50 +0530

    same

--- app/api.py
+++ app/api.py
@@ -216,7 +216,7 @@ async def telegram_webhook(request: Request):
                     f"{TELEGRAM_API}/sendMessage",
                     json={
                         "chat_id": chat_id,
-                        "text": "👇 Choose an option",
+                        "text": "👇 Choose an option to Explore Finux        ",
                         "reply_markup": build_menu(menu_key),
                     },
                 )
