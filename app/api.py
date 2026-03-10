5c1bfb050370c4ed66fcc41a452d2aa8f33a93c6
Tabassum <tabbasum.ss@gmail.com>
2026-03-10 08:25:10 +0530

    more

--- app/api.py
+++ app/api.py
@@ -84,7 +84,7 @@ index = faiss.IndexFlatL2(dimension)
 index.add(np.array(doc_embeddings))
 
 
-def semantic_search(question: str, top_k=3):
+def semantic_search(question: str, top_k=2):
 
     question_vector = embedding_model.encode([question])
 
